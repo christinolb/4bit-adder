0767bcd59cb8d80355781d11d5635fb0817743a1
christinolb <49823328+christinolb@users.noreply.github.com>
2019-12-08 17:03:40 -0500

    Add files via upload

---
+++ 4bitCircuit.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 12:14:21 2019
+
+@author: chris
+"""
+
+class LogicGates():
+    def __init__(self,x,y):
+        self.x=None
+        self.y=None
+        
+
+    
+    
